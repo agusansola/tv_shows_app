45eaaf13992118259e87d93a3f00fe7b4c82da02
agusansola <agustinaansola@gmail.com>
2026-06-25 12:20:03 +0200

    shows provider
    created state management file using provider package.

---
+++ lib/providers/shows_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:tv_shows_app/models/show_model.dart';
+import 'package:tv_shows_app/services/shows_api_service.dart';
+
+class ShowsProvider extends ChangeNotifier {
+  final ShowsApiService _api;
+
+  ShowsProvider(this._api);
+
+  List<ShowModel> _shows = [];
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  List<ShowModel> get shows => _shows;
+  bool get isloading => _isLoading;
+  String? get errorMessage => _errorMessage;
+
+  Future<void> getShows() async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    try {
+      _shows = await _api.fetchShows();
+    } catch (error) {
+      _errorMessage = 'Error loading shows';
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
