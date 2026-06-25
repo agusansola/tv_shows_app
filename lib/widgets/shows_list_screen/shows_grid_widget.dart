e6a08c5e02a5ef97bb932960ce05e3261a11d862
agusansola <agustinaansola@gmail.com>
2026-06-25 13:16:51 +0200

    Shows grid widget
    shows grid widget containing shows card widget

---
+++ lib/widgets/shows_list_screen/shows_grid_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:tv_shows_app/models/show_model.dart';
+import 'package:tv_shows_app/widgets/shows_list_screen/shows_card_widget.dart';
+
+class ShowsGrid extends StatelessWidget {
+  final List<ShowModel> shows;
+
+  const ShowsGrid({super.key, required this.shows});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemCount: shows.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        crossAxisSpacing: 48.0,
+        mainAxisSpacing: 16.0,
+      ),
+      itemBuilder: (context, index) {
+        final show = shows[index];
+
+        return ShowsCard(
+          show: show,
+          onCardTap: () {
+            //TODO: implement on tap nav route to detail screen
+          },
+        );
+      },
+    );
+  }
+}
