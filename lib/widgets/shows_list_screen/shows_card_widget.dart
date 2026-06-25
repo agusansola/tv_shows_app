7eecc3cdbbd71a441cc6038b857354c3900f7f89
agusansola <agustinaansola@gmail.com>
2026-06-25 13:11:23 +0200

    shows card widget
    shows card widget

---
+++ lib/widgets/shows_list_screen/shows_card_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:tv_shows_app/models/show_model.dart';
+
+class ShowsCard extends StatelessWidget {
+  final ShowModel show;
+  final VoidCallback onCardTap;
+
+  const ShowsCard({super.key, required this.show, required this.onCardTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: onCardTap,
+        child: Column(
+          children: [
+            _buildImage(show),
+            Padding(padding: EdgeInsets.only(bottom: 8.0)),
+            Text(
+              show.name,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  show.rating.average.toString(),
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                Icon(Icons.star),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget _buildImage(ShowModel show) {
+  final imageMedium = show.image.medium;
+
+  return Image.network(
+    imageMedium,
+    width: double.infinity,
+    fit: BoxFit.cover,
+    loadingBuilder: (context, child, loadingProgress) {
+      if (loadingProgress == null) {
+        return child;
+      }
+      return SizedBox(
+        width: double.infinity,
+        child: Center(child: CircularProgressIndicator(strokeWidth: 2)),
+      );
+    },
+    errorBuilder: (context, error, stackTrace) {
+      return Container(
+        width: double.infinity,
+        color: Theme.of(context).colorScheme.secondary,
+        child: const Icon(Icons.image_not_supported),
+      );
+    },
+  );
+}
