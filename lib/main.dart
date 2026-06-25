fe61b0974023fd39f23c372a258eb91729c28a63
agusansola <agustinaansola@gmail.com>
2026-06-25 12:24:55 +0200

    provider in main
    Added shows provider at the top of the widget tree since it needs to be accessed by different screens in the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tv_shows_app/app.dart';
+import 'package:tv_shows_app/providers/shows_provider.dart';
+import 'package:tv_shows_app/services/shows_api_service.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ShowsProvider(ShowsApiService())),
+      ],
+      child: const App(),
+    ),
+  );
 }
